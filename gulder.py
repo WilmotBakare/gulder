23d407700f3f9a86340d9fbf180ab8a6de7936c1
wilmot <wilmot.bakare@gmail.com>
2015-06-13 07:16:19 +0100

    function to solve chest problem

--- gulder.py
+++ gulder.py
@@ -1,51 +1,21 @@
 import fileinput, sys
-#import numpy
-from collections import Counter
-
-with open('.input', 'r') as f:
-
- class Chest:
-    pass
-
- max_key = 200
- T = int(f.readline())
- #T_list = list(T)
-
- def solve(chests, keys):
-    keys = 
+import time
 
 
+def main():
+    start = time.clock()
+
+    with open('.input', 'r') as input:
+        T = int(input.readline())
 
 
- for case in range(1,T+1):
-    K, N = [int(x) for x in f.readline().split()]
-    initial_keys = [int(x) for x in f.readline().split()]
-    assert len(initial_keys) == K
-    initial_keys = Counter(initial_keys)
+        with open('.output', 'w') as output:
+            for case in range(1,T+1):
+                K, N = [int(x) for x in input.readline().split()]
 
-    initial_keys = f([initial_keys[key] for key in range(max_key+1)])
+                output.write("Case #%d: %s" % (case, answer))
 
-    chests = list()
+            output.write(str(time.clock() - start))
 
-    for i in range(N):
-        numbers = [int(x) for x in f.readline().split()]
-        Ti = numbers[0]
-        Ki = numbers[1]
-        inside = numbers[2:]
-        assert len(inside) == Ki
-        inside = Counter(inside)
-        inside = f([inside[key] for key in range(max_key+1)])
-        assert inside[0] == 0
+            
 
-        chest = Chest()
-        chest.lock = Ti
-        chest.inside = inside
-        chests.append(chest)
-
-    solution = solve(chests, initial_keys)
-    if solution:
-        answer = " ".join(str(n+1) for n in solution)
-    else:
-        answer = "IMPOSSIBLE"
-
-    print "Case #%d: %s" % (case, answer)
